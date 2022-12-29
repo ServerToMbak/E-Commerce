24f4cfd025442294c987d586ecfeab174b52ae13
ServerToMbak <server78135@gmail.com>
2022-12-29 21:11:49 +0300

    end of section 9

--- E-Commerce/client/src/app/app.component.ts
+++ E-Commerce/client/src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-import { IPagination } from './models/pagination';
-import { IProduct } from './models/product';
+import { IPagination } from './shared/models/pagination';
+import { IProduct } from './shared/models/product';
 
 @Component({
   selector: 'app-root',
@@ -10,17 +10,11 @@ import { IProduct } from './models/product';
 })
 export class AppComponent implements OnInit{
   title = 'SkiNet';
-  products: IProduct[];
 
-  constructor(private http: HttpClient) {}
+  constructor() {}
 
   ngOnInit(): void {
-   this.http.get("https://localhost:5001/api/products?pageSize=50").subscribe(
-    (response: IPagination) =>{
-    this.products = response.data;
-   }, error=>{
-    console.log(error);
-   });
+
   }
 
 }
--- E-Commerce/client/src/app/app.module.ts
+++ E-Commerce/client/src/app/app.module.ts
@@ -4,18 +4,22 @@ import { BrowserAnimationsModule } from "@angular/platform-browser/animations"
 import {HttpClientModule} from "@angular/common/http"
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { CoreModule } from './core/core.module';
+import { ShopModule } from './shop/shop.module';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    NavBarComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    HttpClientModule
+    HttpClientModule,
+    CoreModule,
+    ShopModule,
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
