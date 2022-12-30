5c6ce435357c9774383d1a99275bdf83d93b6a98
ServerToMbak <server78135@gmail.com>
2022-12-30 19:57:50 +0300

    end of section11

--- E-Commerce/client/src/app/app.module.ts
+++ E-Commerce/client/src/app/app.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations"
-import {HttpClientModule} from "@angular/common/http"
+import {HttpClientModule, HTTP_INTERCEPTORS} from "@angular/common/http"
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
 import { ShopModule } from './shop/shop.module';
 import { SharedModule } from './shared/shared.module';
 import { HomeModule } from './home/home.module';
+import { ErrorInterceptor } from './core/Interceptor/error.interceptor';
+
 
 @NgModule({
   declarations: [
@@ -20,9 +22,11 @@ import { HomeModule } from './home/home.module';
     HttpClientModule,
     CoreModule,
     SharedModule,
-    HomeModule
+    HomeModule,
   ],
-  providers: [],
+  providers: [
+    {provide:HTTP_INTERCEPTORS, useClass: ErrorInterceptor,multi :true}
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
