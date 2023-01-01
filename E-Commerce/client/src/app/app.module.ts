6c778578e0b2f71546fe79373feed3fefd2ade90
ServerToMbak <server78135@gmail.com>
2023-01-01 13:16:29 +0300

    End of section 12

--- E-Commerce/client/src/app/app.module.ts
+++ E-Commerce/client/src/app/app.module.ts
@@ -5,10 +5,11 @@ import {HttpClientModule, HTTP_INTERCEPTORS} from "@angular/common/http"
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
-import { ShopModule } from './shop/shop.module';
 import { SharedModule } from './shared/shared.module';
 import { HomeModule } from './home/home.module';
 import { ErrorInterceptor } from './core/Interceptor/error.interceptor';
+import { NgxSpinnerModule } from 'ngx-spinner';
+import { LoadingInterceptor } from './core/Interceptor/loading.interceptor';
 
 
 @NgModule({
@@ -23,9 +24,11 @@ import { ErrorInterceptor } from './core/Interceptor/error.interceptor';
     CoreModule,
     SharedModule,
     HomeModule,
+    NgxSpinnerModule
   ],
   providers: [
-    {provide:HTTP_INTERCEPTORS, useClass: ErrorInterceptor,multi :true}
+    {provide:HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi :true},
+    {provide:HTTP_INTERCEPTORS, useClass: LoadingInterceptor, multi :true}
   ],
   bootstrap: [AppComponent]
 })
